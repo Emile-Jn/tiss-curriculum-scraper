8eb326e0daeb6d0c589735913d3b4a88b7f356c4
Emile-Jn <linters.tamponner_0m@icloud.com>
2024-11-05 14:59:03 +0100

    fixed chrome driver, added tsv file

--- scraper.py
+++ scraper.py
@@ -7,6 +7,26 @@ from selenium.webdriver.safari.service import Service as SafariService
 from selenium.webdriver.support.ui import Select
 import time
 import pandas as pd
+import tqdm
+
+# Constants
+CURRICULUM = pd.DataFrame(columns=['module', 'title', 'code', 'type', 'semester', 'credits'])
+
+SECTION_NAMES = {
+    'Prüfungsfach Data Science - Foundations': 'Foundations',
+    'Prüfungsfach Domain-Specific Aspects of Data Science': 'DSA',
+    'Modul FDS/CO - Fundamentals of Data Science - Core': 'FDS/CO',
+    'Modul FDS/EX - Fundamentals of Data Science - Extension': 'FDS/EX',
+    'Modul MLS/CO - Machine Learning and Statistics - Core': 'MLS/CO',
+    'Modul MLS/EX - Machine Learning and Statistics - Extension': 'MLS/EX',
+    'Modul BDHPC/CO - Big Data and High Performance Computing - Core': 'BDHPC/CO',
+    'Modul BDHPC/EX - Big Data and High Performance Computing - Extension': 'BDHPC/EX',
+    'Modul VAST/CO - Visual Analytics and Semantic Technologies - Core': 'VAST/CO',
+    'Modul VAST/EX - Visual Analytics and Semantic Technologies - Extension': 'VAST/EX',
+    'Prüfungsfach Freie Wahlfächer und Transferable Skills': 'Free Electives'
+}
+SECTION_NAMES_LIST = list(SECTION_NAMES.keys())
+
 
 # TODO: adapt code for other browsers
 # service = SafariService('/usr/bin/safaridriver')
@@ -23,30 +43,15 @@ time.sleep(3)  # Adjust the sleep time if necessary
 
 #%% modules
 
-curriculum = pd.DataFrame(columns=['module', 'title', 'code', 'type', 'semester', 'credits'])
-
-section_names = {
-    'Prüfungsfach Data Science - Foundations': 'Foundations',
-    'Prüfungsfach Domain-Specific Aspects of Data Science': 'DSA',
-    'Modul FDS/CO - Fundamentals of Data Science - Core': 'FDS/CO',
-    'Modul FDS/EX - Fundamentals of Data Science - Extension': 'FDS/EX',
-    'Modul MLS/CO - Machine Learning and Statistics - Core': 'MLS/CO',
-    'Modul MLS/EX - Machine Learning and Statistics - Extension': 'MLS/EX',
-    'Modul BDHPC/CO - Big Data and High Performance Computing - Core': 'BDHPC/CO',
-    'Modul BDHPC/EX - Big Data and High Performance Computing - Extension': 'BDHPC/EX',
-    'Modul VAST/CO - Visual Analytics and Semantic Technologies - Core': 'VAST/CO',
-    'Modul VAST/EX - Visual Analytics and Semantic Technologies - Extension': 'VAST/EX',
-    'Prüfungsfach Freie Wahlfächer und Transferable Skills': 'Free Electives'
-}
-section_names_list = list(section_names.keys())
-
 #%%
 # Locate the semester select element
 semester_select = Select(driver.find_element('name', 'j_id_2i:semesterSelect'))
 
-for semester in semester_select.options:
+for i in range(len(semester_select.options)):
     # Re-locate the semester select element
     semester_select = Select(driver.find_element('name', 'j_id_2i:semesterSelect'))
+    # Get the updated option reference
+    semester = semester_select.options[i]
     # Select each semester by visible text
     semester_select.select_by_visible_text(semester.text)  # TODO: error for switching semester
     print(f'\n Processing the semester {semester.text} \n')
@@ -57,21 +62,21 @@ for semester in semester_select.options:
     n_courses = 0
     section_number = -1 # start with no section
 
-    for i in range(1, len(rows)):  # skip row 0 which just says "Master Data Science"
-        if section_number == -1 and  i > 3:
+    for j in range(1, len(rows)):  # skip row 0 which just says "Master Data Science"
+        if section_number == -1 and  j > 3:
             raise ValueError("Could not find the first section of the curriculum in the first 3 rows.")
         # for each rows, get the 4 grid cells
-        cells = rows[i].find_elements(By.TAG_NAME, "td")
+        cells = rows[j].find_elements(By.TAG_NAME, "td")
         # if the row is a new section of the curriculum, move to that section
         text = cells[0].text.strip()
-        print(f'matching {text} with {section_names_list[section_number+1]}')
-        if text == section_names_list[section_number+1]:
+        # print(f'matching {text} with {section_names_list[section_number+1]}')
+        if text == SECTION_NAMES_LIST[section_number + 1]:
             section_number += 1
-            if section_number > len(section_names) - 2:
+            if section_number > len(SECTION_NAMES) - 2:
                 break
             continue
         # Check if the row contains a hyperlink
-        hyperlinks = rows[i].find_elements(By.TAG_NAME, "a")
+        hyperlinks = rows[j].find_elements(By.TAG_NAME, "a")
         if hyperlinks:  #if there is at least one hyperlink in the row
             # get the course key
             course_key = cells[0].find_element(By.CLASS_NAME, "courseKey").text.strip()
@@ -86,16 +91,17 @@ for semester in semester_select.options:
             # add the course to the curriculum
             # curriculum[keys[section_number]].append(course)
             new_row = pd.DataFrame(
-                {'module': [section_names[section_names_list[section_number]]],
+                {'module': [SECTION_NAMES[SECTION_NAMES_LIST[section_number]]],
                  'title': [course_title],
                  'code': [course_info[0]],
                  'type': [course_info[1]],
                  'semester': [course_info[2]],
                  'credits': [ects]})
-            curriculum = pd.concat([curriculum, new_row], ignore_index=True)
+            CURRICULUM = pd.concat([CURRICULUM, new_row], ignore_index=True)
             n_courses += 1
+            print(f'\r Processed {n_courses} courses.', end='')
 
-    print(f'There were {n_courses} courses found in the curriculum page.')
+    print('Finished.')
 
 #%% Manually add the thesis, seminar and defense, which don't appear as normal tiss courses
 thesis_module = pd.DataFrame(
@@ -106,10 +112,10 @@ thesis_module = pd.DataFrame(
      'semester': ['W and S', 'W and S', 'W and S'],  # Winter and Summer
      'credits': [27, 1.5, 1.5]})
 
-curriculum = pd.concat([curriculum, thesis_module], ignore_index=True)
-curriculum = curriculum.dropna(how='all')  # drop rows with all NaN values
-
+CURRICULUM = pd.concat([CURRICULUM, thesis_module], ignore_index=True)
+CURRICULUM = CURRICULUM.dropna(how='all')  # drop rows with all NaN values
+CURRICULUM.drop_duplicates(inplace=True)
 #%% write the curriculum to a tsv file
-curriculum.drop_duplicates().to_csv('curriculum.tsv', sep='\t', index=False)
+CURRICULUM.to_csv('curriculum.tsv', sep='\t', index=False)
 
 driver.quit()  # close the browser
